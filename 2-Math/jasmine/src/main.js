e241cec9eee7107a8f5ee768cbaea1433f2923cb
spadilla11 <dpadilla26@basecampcodingacademy.org>
2025-09-17 11:21:57 -0500

    submitting 2-math

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -1,2 +1,16 @@
+let ratePerHour = 60;
+let dayRate = 8 * ratePerHour;
 
+let budget = 1000;
+let workdays = Math.floor(budget / dayRate);
+
+let billable = 22;
+let monthlyDiscount = 0.2;
+
+let fullMonths = Math.floor(35 / billable);
+let remainingDays = 35 - billable;
+
+let discountedMonthlyRate = dayRate * billable * (1 - monthlyDiscount);
+let remainingCost = remainingDays * dayRate;
+let totalProjectCost = Math.ceil(discountedMonthlyRate + remainingCost);
   
